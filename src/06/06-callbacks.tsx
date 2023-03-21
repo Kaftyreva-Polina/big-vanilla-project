87b08198496ef45b0ed6b495618f52797fecbdc1
KaftyrevaPolina <pkaft.13.8@gmail.com>
2023-03-22 00:08:54 +0300

    06 callbacks

---
+++ src/06/06-callbacks.tsx
@@ -0,0 +1,39 @@
+import React, {ChangeEvent, MouseEvent} from "react";
+
+// const callback = (): number => {
+//     alert("hey")
+//     return 12
+// }
+//
+// window.setTimeout(callback, 1000)
+
+export const User = () => {
+
+    const deleteUser = (event: MouseEvent<HTMLButtonElement>) => {
+        // event.currentTarget.name
+        alert(event.currentTarget.name)
+    }
+
+    // const saveUser = () => {
+    //     alert("user have been saved")
+    // }
+
+    const onNameChanged = () => {
+        console.log("name changed")
+    }
+
+    const onAgeChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log("age changed" + event.currentTarget.value)
+    }
+
+    const focusLostHandler = () => {
+        console.log("focus lost")
+    }
+
+    return <div><textarea onChange={onNameChanged} onBlur={focusLostHandler}>Dimych</textarea>
+
+        <input onChange={onAgeChanged} type={"number"}/>
+        <button name="delete" onClick={deleteUser}>x</button>
+        <button name="save" onClick={deleteUser}>x</button>
+    </div>
+}
